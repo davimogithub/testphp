958436a5bd4564f73d290470576ac3e5e9421f99
davimogithub <68911057+davimogithub@users.noreply.github.com>
2020-08-09 16:27:34 +0430

    Create fatch.php

---
+++ fatch.php
@@ -0,0 +1,19 @@
+<?php 
+if (isset($_POST['order_id']) && $_POST['order_id']!="") { 
+$order_id = $_POST['order_id']; 
+$url = "http://davimotest.ss0.ir/rest/api/".$order_id;
+
+$client = curl_init($url); 
+curl_setopt($client,CURLOPT_RETURNTRANSFER,true); 
+$response = curl_exec($client); 
+
+$result = json_decode($response); 
+
+echo "<table>"; 
+echo "<tr><td>Order ID:</td><td>$result->order_id</td></tr>"; 
+echo "<tr><td>Amount:</td><td>$result->amount</td></tr>"; 
+echo "<tr><td>Response Code:</td><td>$result->response_code</td></tr>"; 
+echo "<tr><td>Response Desc:</td><td>$result->response_desc</td></tr>"; 
+echo "</table>"; 
+} 
+?>
